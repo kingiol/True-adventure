5da61e5f5c1c530d683de3628b0daab3670e7725
kingiol <kingxiaokang@gmail.com>
2013-04-09 22:18:58 +0800

    init base framework

---
+++ True-adventure/TAAdventureViewController.h
@@ -0,0 +1,13 @@
+//
+//  TAAdventureViewController.h
+//  True-adventure
+//
+//  Created by Kingiol on 13-4-9.
+//  Copyright (c) 2013年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAAdventureViewController : UIViewController
+
+@end
---
+++ True-adventure/TAAdventureViewController.m
@@ -0,0 +1,38 @@
+//
+//  TAAdventureViewController.m
+//  True-adventure
+//
+//  Created by Kingiol on 13-4-9.
+//  Copyright (c) 2013年 Kingiol. All rights reserved.
+//
+
+#import "TAAdventureViewController.h"
+
+@interface TAAdventureViewController ()
+
+@end
+
+@implementation TAAdventureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
